982bd931558f7a1a8c1db6736f02c36ec2c51800
Igor Vasyliev <vasylievigorweb@gmail.com>
2020-05-18 11:44:45 +0300

    new version

--- module/catalog/controller/extension/module/[modulename].php
+++ module/catalog/controller/extension/module/[modulename].php
@@ -3,6 +3,8 @@ class ControllerExtensionModule[modulename] extends Controller {
 	public function index() {
 		$this->load->language('extension/module/[modulename]');
 
+		$data['heading_title'] = $this->language->get('heading_title');
+
 		return $this->load->view('extension/module/[modulename]', $data);
 	}
 }
